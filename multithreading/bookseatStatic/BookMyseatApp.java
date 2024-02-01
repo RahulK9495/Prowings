0bac40eeb2fbf235e15b5b3801f769ec4e706893
RahulK9495 <rahulkhot5554@gmail.com>
2024-02-01 10:31:05 +0530

    new commit

--- multithreading/bookseatStatic/BookMyseatApp.java
+++ multithreading/bookseatStatic/BookMyseatApp.java
@@ -4,24 +4,24 @@ public class BookMyseatApp {
 
 	public static void main(String[] args) {
 
-		Theater theater1=new Theater();
-		Theater theater2=new Theater();
-		
-		MyThreadOne t1=new MyThreadOne(theater1, 3); 
-		MyThreadTwo t2=new MyThreadTwo(theater1, 4); 
-		
+		Theater theater1 = new Theater();
+		Theater theater2 = new Theater();
+
+		MyThreadOne t1 = new MyThreadOne(theater1, 3);
+		MyThreadTwo t2 = new MyThreadTwo(theater1, 4);
+
 		t1.setName("Ram");
 		t2.setName("Sham");
-		
+
 		t1.start();
 		t2.start();
 
-		MyThreadOne t3=new MyThreadOne(theater2, 1); 
-		MyThreadTwo t4=new MyThreadTwo(theater2, 6); 
-	
+		MyThreadOne t3 = new MyThreadOne(theater2, 1);
+		MyThreadTwo t4 = new MyThreadTwo(theater2, 6);
+
 		t3.setName("abhi");
 		t4.setName("rahul");
-		
+
 		t3.start();
 		t4.start();
 
--- multithreading/bookseatStatic/Theater.java
+++ multithreading/bookseatStatic/Theater.java
@@ -1,25 +1,22 @@
 package prowings.multithreading.bookseatStatic;
 
 public class Theater {
-	
+
 	static int totalseat = 10;
-	
-	public static synchronized void bookSeats( int seats)
-	{
-		
-	
+
+	public static synchronized void bookSeats(int seats) {
+
 		{
-			if (seats <= totalseat )
-			{
-				System.out.println(seats + "  Seats booked successfully by :  "+Thread.currentThread().getName());
-				totalseat=totalseat-seats;
-				System.out.println("Remaining seats after booking : "+totalseat);
-			}
-			else
-			{
-				System.out.println("Can Not book : "+seats +"Seats....Available seats are : "+totalseat );
+			if (seats <= totalseat) {
+				System.out.println(seats + "  Seats booked successfully by :  " + Thread.currentThread().getName());
+				totalseat = totalseat - seats;
+				System.out.println("Remaining seats after booking : " + totalseat);
+			} else {
+				System.out.println("Can Not book : " + seats + "Seats....Available seats are : " + totalseat);
 			}
 		}
 	}
+	int i=0;
+	int j=0;
 
 }
